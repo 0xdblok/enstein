0ffc16e163d027262003fe5687b41a12124288c8
0xdblok <dblok0x@gmail.com>
2024-03-28 15:23:50 +0100

    ee

---
+++ app/component/another.js
@@ -0,0 +1,27 @@
+"use client";
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Another() {
+  return (
+    <div>
+      <div className="  w-full h-0 shadow-[0_2px_8px_0_rgba(63,69,81,0.16)] overflow-hidden will-change-transform mt-[1.6em] mb-[0.9em] pt-[56.2225%] pb-0 rounded-lg hover:blur-sm hover:brightness-50 hover:sepia hover:contrast-100 hover:hue-rotate-30 hover:invert-0 hover:opacity-5 hover:saturate-150 hover:text-orange-400 hover:text-xl disabled:w-3/12 disabled:text-[color:var(--some-color)] disabled:text-[1em] md:inset-x-1/4 md:inset-y-auto supports-[display:grid]:grid supports-[display:grid]:col-span-1;">
+        <iframe
+          loading="lazy"
+          className="absolute w-full h-full m-0 p-0 border-[none] left-0 top-0"
+          src="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGAxS5FUn8&#x2F;GpvRbfNSrlTo-UL-q-Rmcw&#x2F;view?embed"
+          allowfullscreen="allowfullscreen"
+          allow="fullscreen"></iframe>
+      </div>
+      <a
+        href="https:&#x2F;&#x2F;www.canva.com&#x2F;design&#x2F;DAGAxS5FUn8&#x2F;GpvRbfNSrlTo-UL-q-Rmcw&#x2F;view?utm_content=DAGAxS5FUn8&amp;utm_campaign=designshare&amp;utm_medium=embeds&amp;utm_source=link"
+        target="_blank"
+        rel="noopener">
+        Light Beige Sleek and Simple Blogger Personal Website
+      </a>
+    </div>
+  );
+} /*position:relative; width: 100%; height: 0; padding-top: 56.2225%;
+    padding-bottom: 0; box-shadow: 0 2px 8px 0 rgba(63,69,81,0.16); margin-top: 1.6em; margin-bottom: 0.9em; overflow: hidden;
+    border-radius: 8px; will-change: transform
+ */
+/*position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0;margin: 0;*/
--- app/page.js
+++ app/page.js
@@ -5,9 +5,12 @@ import Footer from "./component/footer";
 import Plan from "./component/plan";
 import Cooking from "./component/cooking";
 
+import Another from "./component/another";
+
 export default function Home() {
   return (
     <main className="bg-[#E7DFD8]">
+      <Another />
       <Navbar />
       <Hero />
       <Plan />
